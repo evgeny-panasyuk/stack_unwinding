17589f2034233cc9b727677e1f3cd0eedba49454
Evgeny Panasyuk <Evgeny.Panasyuk@gmail.com>
2012-09-29 00:34:58 +0400

    re-shape of example_from_dlang_dot_org's code

--- examples/example_from_dlang_dot_org.cpp
+++ examples/example_from_dlang_dot_org.cpp
@@ -8,11 +8,11 @@
 using namespace stack_unwinding;
 using namespace std;
 
-class Print
+class write
 {
     std::string str_;
 public:
-    Print(const std::string &str): str_(str){}
+    write(const std::string &str): str_(str){}
     void operator()() const
     {
         cout << str_;
@@ -21,14 +21,8 @@ public:
 
 struct Foo
 {
-    Foo()
-    {
-        Print("0")();
-    }
-    ~Foo()
-    {
-        Print("1")();
-    }
+    Foo() { write("0")(); }
+    ~Foo() { write("1")(); }
 };
 void func_throw(int i)
 {
@@ -60,14 +54,14 @@ int main(int,char *[])
 */
     try
     {   // need more syntactic sugar: UNIQUE_NAME macro, maybe boost::function, etc, etc..
-        scope_action a=make<scope_exit>(Print("2"));
-        scope_action b=make<scope_success>(Print("3"));
+        scope_action a=make<scope_exit>(write("2"));
+        scope_action b=make<scope_success>(write("3"));
         Foo c;
-        scope_action d=make<scope_failure>(Print("4"));
-        func_throw(1); // unreachiable warrnings prevention
-        scope_action e=make<scope_exit>(Print("5"));
-        scope_action f=make<scope_success>(Print("6"));
-        scope_action g=make<scope_failure>(Print("7"));
+        scope_action d=make<scope_failure>(write("4"));
+        func_throw(1); // unreachiable warnings prevention
+        scope_action e=make<scope_exit>(write("5"));
+        scope_action f=make<scope_success>(write("6"));
+        scope_action g=make<scope_failure>(write("7"));
 
         (void)a;(void)b;(void)c;(void)d;(void)e;(void)f;(void)g; // unused warnings prevention
     }
