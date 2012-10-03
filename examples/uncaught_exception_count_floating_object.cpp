42f71271983d4b1be5e6526f27505572f72e787b
Evgeny Panasyuk <Evgeny.Panasyuk@gmail.com>
2012-10-03 14:33:54 +0400

    Added example when current unwinding indicator can be folled

---
+++ examples/uncaught_exception_count_floating_object.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+#include <stack_unwinding.hpp>
+
+#include "examples_common.hpp"
+
+using namespace std;
+using namespace stack_unwinding;
+
+struct ExpCountPrinter
+{
+    ExpCountPrinter()
+    {
+        cout << "uncaught_exception_count=" << uncaught_exception_count() << endl;
+    }
+    ~ExpCountPrinter()
+    {
+        cout << "uncaught_exception_count=" << uncaught_exception_count() << endl;
+    }
+};
+
+ExpCountPrinter *global_ptr=0;
+
+struct DtorStuff
+{
+    ~DtorStuff()
+    {
+        global_ptr=new ExpCountPrinter;
+    }
+};
+
+int main(int,char *[])
+{
+    try
+    {
+        DtorStuff d;
+        throw 1;
+    }
+    catch(int) {}
+
+    delete global_ptr;
+
+    return 0;
+}
+
+ExpectedStdoutTest test_cout
+(
+    "uncaught_exception_count=1\n"
+    "uncaught_exception_count=0\n"
+);
