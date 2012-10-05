dab12f44e4d41ac547b6ddb06bf8d54f0e85d7ae
Evgeny Panasyuk <Evgeny.Panasyuk@gmail.com>
2012-10-06 03:55:46 +0400

    Added uncaught_exception_count_stress_test - 1024 simultanious uncaught exceptions

---
+++ examples/uncaught_exception_count_stress_test.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+#include <stack_unwinding.hpp>
+
+using namespace std;
+
+vector<unsigned> exception_counts;
+const unsigned test_count=1024;
+
+struct ExpCountSaver
+{
+    ~ExpCountSaver()
+    {
+        exception_counts.push_back(stack_unwinding::uncaught_exception_count());
+    }
+};
+
+
+template<int n>
+struct ExptSwallower
+{
+    ~ExptSwallower()
+    {
+        try
+        {
+            ExpCountSaver p;
+            ExptSwallower<n-1> a;
+            throw 1;
+            (void)p;(void)a; // unused warnings prevention
+        }catch(int){}
+    }
+};
+template<>
+struct ExptSwallower<0>
+{
+    ~ExptSwallower() {}
+};
+int main(int,char *[])
+{
+    exception_counts.reserve(test_count);
+    try
+    {
+        ExptSwallower<test_count> test;
+    } catch(int) {}
+    if(exception_counts.size()!=test_count)
+        return 1;
+    adjacent_difference(exception_counts.rbegin(),exception_counts.rend(),exception_counts.rbegin());
+    if
+    (
+        find_if(exception_counts.begin(),exception_counts.end(), bind2nd(not_equal_to<unsigned>(),1) )
+            !=
+        exception_counts.end()
+    )
+        return 2;
+    return 0;
+}
